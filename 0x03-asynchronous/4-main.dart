bcf19a63b0732b5e86f3731c808866ad0c83fca8
“AAndrews-1982” <altonandrews@icloud.com>
2024-10-08 12:22:34 -0500

    added double to main file

--- 0x03-asynchronous/4-main.dart
+++ 0x03-asynchronous/4-main.dart
@@ -1,7 +1,10 @@
 import '4-get_sum.dart';
 
-
 main() async {
-  print(await calculateTotal());
+  double result = await calculateTotal();
+  if (result == -1.0) {
+    print(result.toInt()); // Print -1 as an integer
+  } else {
+    print(result);
+  }
 }
-
